232593134a487545f278b9785311df5c2b8e3dee
yandre13 <yandre13.elsf@gmail.com>
2023-04-05 02:44:23 -0500

    xata db

--- netlify/functions/get-hello.ts
+++ netlify/functions/get-hello.ts
@@ -1,3 +1,4 @@
+import { getSubs } from '@/lib/xata.subs'
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
 
 const handler: Handler = async (
@@ -5,18 +6,19 @@ const handler: Handler = async (
   context: HandlerContext
 ) => {
   console.log('Hello from the console!')
-  console.log('Event:', event)
-  console.log('Context:', context)
-  const joke = await fetch('https://icanhazdadjoke.com/', {
-    headers: {
-      Accept: 'application/json',
-    },
-  })
+  // console.log('Event:', event)
+  // console.log('Context:', context)
+  // const joke = await fetch('https://icanhazdadjoke.com/', {
+  //   headers: {
+  //     Accept: 'application/json',
+  //   },
+  // })
+  const subs = await getSubs()
 
-  const jsonData = await joke.json()
+  // const jsonData = await joke.json()
   return {
     statusCode: 200,
-    body: JSON.stringify(jsonData),
+    body: JSON.stringify(subs),
   }
 }
 
